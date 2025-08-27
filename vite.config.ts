7ceddd16ed9b712299df462b99c1c9949336ee18
wanderconnet-hub <wanderconnect9211@gmail.com>
2025-08-28 02:06:44 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,1 @@
-// File: vite.config.ts - FINAL SIMPLE VERSION
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+/// <reference types="vite/client" />
